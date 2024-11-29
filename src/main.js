94cd3e80345bbd849b614f9690912c63e493fcfb
amnarafi <s.amnarafi1999@gmail.com>
2024-11-29 16:17:47 +0500

    cara shop is added

---
+++ src/main.js
@@ -0,0 +1,12 @@
+
+import './style.css';
+
+import products from '/api/products.json';
+import { showProductContainer } from '../homeProductCards';
+
+
+// call the function to display all products
+
+showProductContainer(products);
+
+
